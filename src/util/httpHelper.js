d31196638864841b2f5d1cc455bede1e69e2b844
Tobias Hilbrich <tobias.hilbrich@gmail.com>
2018-01-19 21:55:16 +0100

    format and complete tests for http helper get

--- src/util/httpHelper.js
+++ src/util/httpHelper.js
@@ -1,18 +1,21 @@
 "use strict";
 
 function get(url, requestImpl = new XMLHttpRequest()) {
-  return new Promise((resolve, reject) => {
-    requestImpl.onreadystatechange = function() {
-      if (requestImpl.readyState == 4 && requestImpl.status == 200) {
-        resolve(requestImpl.responseText);
-      } else {
-        reject(new Error("Failed with status " + requestImpl.status));
-      }
-    };
+    return new Promise((resolve, reject) => {
+        requestImpl.onreadystatechange = function() {
+            if (requestImpl.readyState == 4) {
+                if (requestImpl.status == 200) {
+                    resolve(requestImpl.responseText);
 
-    requestImpl.open("GET", url, true);
-    requestImpl.send(null);
-  });
+                } else {
+                    reject(new Error("Failed with status " + requestImpl.status));
+                }
+            }
+        };
+
+        requestImpl.open("GET", url, true);
+        requestImpl.send(null);
+    });
 }
 
-export {get};
+module.exports = {get};
--- test/util/httpHelper.mocha.js
+++ test/util/httpHelper.mocha.js
@@ -4,48 +4,71 @@ const HttpHelper = require("../../src/util/httpHelper");
 const assert = require("chai").assert;
 const sinon = require("sinon");
 
-describe("HttpHelper", () => {
-  describe("get", () => {
-    const request = {};
-    let promise;
+describe('HttpHelper', () => {
+    describe('get', () => {
+        const request = {};
+        let promise;
 
-    beforeEach(function() {
-      request.open = sinon.spy();
-      request.send = sinon.spy();
-      request.status = 200;
-      request.readyState = 4;
+        beforeEach(function() {
+            request.open = sinon.spy();
+            request.send = sinon.spy();
+            request.status = 200;
+            request.readyState = 4;
 
-      promise = HttpHelper.get("example.com", request);
+            promise = HttpHelper.get('example.com', request);
+        });
+
+        it('should assign an onreadystatechange callback on the http request', () => {
+            assert.isFunction(request.onreadystatechange);
+        });
+
+        it('should call send and open on the request', () => {
+            assert.ok(request.open.called);
+            assert.ok(request.send.called);
+        });
+
+        it('should resolve when onreadystatechange is triggered with ready state 4', () => {
+            const promise = HttpHelper.get('example.com', request);
+            request.onreadystatechange();
+
+            return promise;
+        });
+
+        it('should reject if status is not 200', async () => {
+            const promise = HttpHelper.get('example.com', request);
+            request.status = 400;
+            request.onreadystatechange();
+
+            let didAssertionFail = false;
+            try {
+                await promise;
+            } catch (e) {
+                didAssertionFail = true;
+            }
+
+            assert.ok(didAssertionFail);
+        });
+
+        it('should return the response from the Http get request', async () => {
+            const promise = HttpHelper.get('example.com', request);
+            const expectedResponse = 'Hello World!';
+            request.responseText = expectedResponse
+            request.onreadystatechange();
+
+            await promise.then((response) => assert.ok(response === expectedResponse));
+        });
+
+        it('should do nothing for onreadystatechange with ready state != 4', async () => {
+            const promise = HttpHelper.get('example.com', request);
+            request.readyState = 1;
+            request.onreadystatechange();
+
+            const expectedResponse = 'Hello World!';
+            request.responseText = expectedResponse
+            request.readyState = 4;
+            request.onreadystatechange();
+
+            await promise.then((response) => assert.ok(response === expectedResponse));
+        });
     });
-
-    it("should assign an onreadystatechange callback on the http request",
-       () => { assert.isFunction(request.onreadystatechange); });
-
-    it("should call send and open on the request", () => {
-      assert.ok(request.open.called);
-      assert.ok(request.send.called);
-    });
-
-    it("should resolve when onreadystatechange is triggered", () => {
-      const promise = HttpHelper.get("example.com", request);
-      request.onreadystatechange();
-
-      return promise;
-    });
-
-    it("should reject if status is not 200", async () => {
-      const promise = HttpHelper.get("example.com", request);
-      request.status = 400;
-      request.onreadystatechange();
-
-      let didAssertionFail = false;
-      try {
-        await promise;
-      } catch (e) {
-        didAssertionFail = true;
-      }
-
-      assert.ok(didAssertionFail);
-    });
-  });
 });
