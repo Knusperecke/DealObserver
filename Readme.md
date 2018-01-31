7c3c27729318fbabb1d5de89c7e12592805156a5
Tobias Hilbrich <tobias.hilbrich@gmail.com>
2018-01-31 21:58:14 +0100

    setup mysql connection play with table creation

--- Readme.md
+++ Readme.md
@@ -1,9 +1,35 @@
-Research:
+# Canyon Grabber: A Tool to Observe Deals 
 
-Get Factory Outlet items:
+## Installation 
 
+Preqs:
+- Install node
+
+Database Stuff (OSX):
+```
+#Database Stuff
+brew install mysql
+brew services start mysql
+mysql_secure_installation ##PinkiePie
+
+#launch SequelPro
+# create database "canyon"
+```
+
+Install and Run:
+```
+npm i
+npm run run
+```
+
+## Initial Research
+
+### Get Factory Outlet items
+```
 curl 'https://www.canyon.com/en/factory-outlet/ajax/articles.html?category=triathlon&type=html' -XGET > /Users/saibot/Downloads/canyon2.txt 
+```
 
-Some Parsing:
-
+### Some Parsing
+```
 cat /Users/saibot/Downloads/canyon2.txt | grep "\"name\"" && cat /Users/saibot/Downloads/canyon2.txt | grep "\"price\":" && cat /Users/saibot/Downloads/canyon2.txt | grep "data-size="
+```
--- src/grabber/grabber.js
+++ src/grabber/grabber.js
@@ -3,8 +3,48 @@
 const CanyonFetcher = require('./canyon/fetcher');
 const CanyonParser = require('./canyon/parser');
 const Logger = require('../util/logger');
+const MySql = require('mysql');
 
 function run() {
+    const connection =
+        MySql.createConnection({host: 'localhost', user: 'root', password: 'PinkiePie', database: 'canyon'});
+    connection.connect((error) => {
+        if (error) {
+            throw new Error('Failed in database connection: ' + error);
+        }
+
+        Logger.log('Connected to databse');
+    });
+    connection.query('SELECT 1 + 1 AS solution', function(error, results) {
+        if (error) {
+            throw new Error('Failed in query: ' + error);
+        }
+        Logger.log('The solution is: ', results[0].solution);
+    });
+    connection.query(
+        'CREATE TABLE IF NOT EXISTS Items (' +
+            'ID INT NOT NULL,' +
+            'name VARCHAR(255) NOT NULL,' +
+            'price DOUBLE,' +
+            'offerId VARCHAR(255) NOT NULL,' +
+            'size VARCHAR(64) NOT NULL,' +
+            'modelYear INT,' +
+            'isPermanent TINYINT,' +
+            'foundTime DATETIME,' +
+            'lostTime DATETIME,' +
+            'foundCount INT' +
+            ')',
+        (error) => {
+            if (error) {
+                throw new Error('Failed in query: ' + error);
+            }
+        });
+    connection.end((error) => {
+        if (error) {
+            throw new Error('Failed to close database connection: ' + error);
+        }
+    });
+
     CanyonFetcher().forEach((query) => {
         query.then(CanyonParser).then((items) => {
             Logger.log(items);
