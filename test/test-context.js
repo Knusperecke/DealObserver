f1ff3ffd9723dad920804c9a3564c75b2bbf91be
Tobias Hilbrich <tobias.hilbrich@gmail.com>
2018-03-05 21:28:00 +0100

    Exclude database tests from webpack tests

--- test/test-context.js
+++ test/test-context.js
@@ -1,4 +1,4 @@
 require('babel-polyfill');
 
-var context = require.context('./', true, /\.mocha\.js$/);
+var context = require.context('./', true, /(grabber|notifier|util|viewer)\/.*\.mocha\.js$/);  // (?!database)
 context.keys().forEach(context);
